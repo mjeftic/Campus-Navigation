16f21b7f48fbe0f9636bacbd08e025500d262d95
Marko <mjeftic@mail.uni-mannheim.de>
2016-10-03 15:17:30 +0200

    added bottom navigation

--- app/src/main/java/unima/campus_navigation/MainActivity.java
+++ app/src/main/java/unima/campus_navigation/MainActivity.java
@@ -1,13 +1,40 @@
 package unima.campus_navigation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
+
+	CoordinatorLayout coordinatorLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+		coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
+		bottomBar.setItemsFromMenu(R.menu.menu, new OnMenuTabSelectedListener() {
+			@Override
+			public void onMenuItemSelected(int itemId) {
+				switch (itemId) {
+					case R.id.location_item:
+						//Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
+						break;
+					case R.id.favorite_item:
+						//Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
+						break;
+				}
+			}
+		});
+
+		// Set the color for the active tab. Ignored on mobile when there are more than three tabs.
+		bottomBar.setActiveTabColor("#FF4081");
+
+
 	}
+
 }
